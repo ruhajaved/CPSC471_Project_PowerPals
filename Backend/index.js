42cffe920dfca538e39824a203b3359010215b22
MustafaMuhammad2000 <mustafamuhammad0508@gmail.com>
2023-04-03 23:11:28 -0600

    Lots of changes, read PR

--- Backend/index.js
+++ Backend/index.js
@@ -11,6 +11,6 @@ app.use(cors());
 app.use("/api/admin", adminRoutes);
 
 // Start the server
-app.listen(3000, () => {
+app.listen(8000, () => {
   console.log("Server listening on port 3000");
 });
