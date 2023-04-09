dc06ea381685ff31e8081f56666676a81b38f5db
JJMyoshi <86997584+JJMyoshi@users.noreply.github.com>
2023-04-08 19:46:09 -0600

    Added HomePage changes

--- my-app/src/Customer/CustomerHomePage.js
+++ my-app/src/Customer/CustomerHomePage.js
@@ -1,18 +1,61 @@
+
+
+import SeeAllGyms from "./SeeAllGyms";
+
 import { useState } from "react";
 
+
 function CustomerHomePage() {
-  const [content, setContent] = useState(null); // double check this
+  const [content, setContent] = useState(null); // default to gym content
 
   const handleContentChange = (newContent) => {
     setContent(newContent);
   };
 
   const renderContent = () => {
-    return;
+    switch (content) {
+      case "gym":  // THIS IS THE LEFT HEADER
+        return (
+          <div>
+
+          </div>
+          // TYPE IN THE "FUNCTIONS HERE"     <AddInstructor />
+        );
+      case "fitness_class": // THIS IS THE LEFT HEADER
+        return (
+          <div> 
+              < SeeAllGyms />
+            
+          </div>
+          // TYPE IN THE "FUNCTIONS HERE"     <AddInstructor />
+        );
+        case "membership": // THIS IS THE LEFT HEADER
+        return (
+          <div> 
+              < SeeAllGyms />
+            
+          </div>
+          // TYPE IN THE "FUNCTIONS HERE"     <AddInstructor />
+        );
+      default:
+        return null;
+    }
   };
 
   return (
     <div>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div
+          style={{
+            backgroundColor: "#f2f2f2",
+            height: "100vh",
+            width: "15%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <button
             style={{
               backgroundColor: "transparent",
@@ -22,12 +65,41 @@ function CustomerHomePage() {
               padding: "10px",
               cursor: "pointer",
             }}
-            //onClick={() => handleContentChange("gym")}
+            onClick={() => handleContentChange("gym")}
           >
-            customer
+            View All Gyms
           </button>
+          <button
+            style={{
+              backgroundColor: "transparent",
+              border: "none",
+              fontSize: "16px",
+              margin: "10px",
+              padding: "10px",
+              cursor: "pointer",
+            }}
+            onClick={() => handleContentChange("fitness_class")}
+          >
+            View All Classes
+          </button>
+          <button
+            style={{
+              backgroundColor: "transparent",
+              border: "none",
+              fontSize: "16px",
+              margin: "10px",
+              padding: "10px",
+              cursor: "pointer",
+            }}
+            onClick={() => handleContentChange("membership")} // SIGN UP FOR MEMBERSHIP HERE
+          >
+            Sign Up For Membership            
+          </button> 
+          
+        </div>
+        <div className="content">{renderContent()}</div>
+      </div>
     </div>
   );
 }
-
 export default CustomerHomePage;
