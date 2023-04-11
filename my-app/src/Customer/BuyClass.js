f0be45dcad463dbf10d88f634f4d8601ca2d6aee
JJMyoshi <86997584+JJMyoshi@users.noreply.github.com>
2023-04-10 23:59:18 -0600

    Cleaned up Syntax

--- my-app/src/Customer/BuyClass.js
+++ my-app/src/Customer/BuyClass.js
@@ -17,8 +17,8 @@ function BuyClass(eachClass) {
     }
 
     const handleBuy = (close) => {
+        // if credit card no is empty, do not allow user to pay for class
         if (creditCardNo === "") {return;}
-
 
         const requestBody = {
           classId: eachClass.eachClass.Class_ID,
@@ -34,10 +34,8 @@ function BuyClass(eachClass) {
         })
           .then((response) => response.json())
           .then((data) => { 
-                            //close();  
                             setSuccess(true); 
                             setTimeout(() => {window.location.reload();}, 3000);
-                          
                             console.log(data); })
           .catch((error) => console.error(error));
     };
@@ -115,7 +113,7 @@ function BuyClass(eachClass) {
                       padding: "10px",
                     }}
                   >
-                    Classes Added! Please see Class Payment for Confirmaton.
+                    Class Added! Please see Class Payment for Confirmation.
                   </h3>
                 )}
               </form>
--- my-app/src/Customer/PaymentList.js
+++ my-app/src/Customer/PaymentList.js
@@ -4,7 +4,6 @@ import { CustomerContext } from "../Context/CustomerContext";
 function PaymentList() {
     const { CustomerID } = useContext(CustomerContext);
     const [payments, setPayments] = useState([]);
-
     useEffect(() => {
       const fetchData = async () => {
         const response = await fetch(
@@ -21,7 +20,6 @@ function PaymentList() {
       };
       fetchData();
     }, []);
-  
     return (
       <div style={{ margin: "auto", maxWidth: "80%" }}>
 
